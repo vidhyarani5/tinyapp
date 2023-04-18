a0405f2dc28832b3987034c498ad0c9c03e58542
Vidhyarani pinumalla <vidhyarani.pinumalla@hotmail.com>
2023-04-18 18:01:24 -0400

    added another api

--- express_server.js
+++ express_server.js
@@ -14,6 +14,11 @@ app.get("/", (req, res) => {
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+
 
 
 
