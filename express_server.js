49e1d1eba1ac2f24f5199274166e467c908baa8f
Vidhyarani pinumalla <vidhyarani.pinumalla@hotmail.com>
2023-04-19 22:09:32 -0400

    Test the New Route /urls/:id

--- express_server.js
+++ express_server.js
@@ -39,7 +39,7 @@ app.get("/hello", (req, res) => {
   const templateVars = { greeting: "Hello World!" };
   res.render("hello_world", templateVars);
 });
-/*app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL:  };
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
-});*/
+});
